166695e6ac926a4d57ca17c86382daab4f1861e3
Jacob Skinner <skinnej3@wwu.edu>
2018-05-08 12:05:57 -0700

    project work

--- project/project.py
+++ project/project.py
@@ -11,61 +11,36 @@ then steps into a velocity verlet loop until complete.
 All internal calculations are in SI units.
 '''
 
+def accelerator(r, r_venus, t):
+    # Calculates the acceleration due to Sol and Venus.
 
-array = np.array
+    # Find the acceleration due to venus
+    venus_sep      = r-r_venus
+    venus_sep_norm = np.norm(sep)
+    
+    # A = GM/R^2.
+    venus_accel  = sep*(6.67428e-11*4.867488088236164e+24)/(sep_norm**3)
 
-# Masses of the 2 bodies. (kg)
-m_sun, m_venus = 1.989e30, 4.8675e24
+    sol_sep      = -r
+    sol_sep_norm = np.norm(sep)
+    sol_accel    = sep*(6.67428e-11*1.989e30)/(sep_norm**3)
 
-# Use the reduced mass to simplify the 2-body calculations.
-m_venus = m_sun*m_venus/(m_sun + m_venus)
-
-# Eccentricity of Venus
-e_venus = 0.006772
-
-# Semimajor axis of Venus
-a_venus = 1.49598023e11
-
-
-# Initial positions of the Venus.
-# Venus is initialized at its aphelion, the sun is stationary at the origin.
-r_venus  = array([1.08939e11, 0])
-
-v_venus = array([0, ((6.67428e-11*m_sun)*((1-e_venus)/(a_venus*(1+e_venus)))*(1+m_earth/m_sun))**(1/2)])
-
-# Store this info in a single variable (easier to pass to function).
-venus = [m_venus, r_venus, v_venus]
-
-def accelerator(norm, m, r, r0):
-    '''
-    Calculates the acceleration due to Gravity
-    that an object feels from another.
-
-    e.g. m is mass of earth, r0 is your position, r is the position of earth
-    '''
-
-    # Find the sum of the squares of the components of the
-    # separation vector for the two objects.
-    sep = r-r0
-    sep_norm = norm(sep)
-
-    # return F = GM/R^2
-    return   sep*(6.67428e-11*m)/(sep_norm**3)
-def pathFinder(T, dt, venus, swarm):
+    return venus_accel + sol_accel
+def pathFinder(T, dt, swarm):
     '''
     Takes in a run time, delta-t, and info for 3 bodies.
     Returns timestamped coordinates for the specified run time.
     '''
     
-    # Optimization?
-    norm = np.linalg.norm
+    # Initial velocity of Venus.
+    r_venus  = np.array([1.08208e11, 0])
+
+    # Period of Venus.
+    p_venus = 1.941436081e7
     
     # Convert T from years to seconds.
     T = T*31556926
     t = 0
-
-    m_venus, r_venus, v_venus = venus[0], venus[1], venus[2]
-    m_sun,   r_sun,   v_sun   = 1.989e30, array([0, 0]), array([0, 0])
     
     paths = np.empty((round(T/dt), 7))
 
@@ -75,13 +50,13 @@ def pathFinder(T, dt, venus, swarm):
 
     # Use Heun's method to update positions of each body.
     p1_bar = old_p1 + old_v1*dt
-    p1 = old_p1 + (dt/2)*(2*old_v1+(accelerator(norm, m2, old_p2, p1_bar) + accelerator(norm, m3, old_p3, p1_bar))*dt)
+    p1 = old_p1 + (dt/2)*(2*old_v1+accelerator(m3, old_p3, p1_bar)*dt)
 
     p2_bar = old_p2 + old_v2*dt
-    p2 = old_p2 + (dt/2)*(2*old_v2+(accelerator(norm, m1, old_p1, p2_bar) + accelerator(norm, m3, old_p3, p2_bar))*dt)
+    p2 = old_p2 + (dt/2)*(2*old_v2+accelerator(norm, m3, old_p3, p2_bar)*dt)
 
     p3_bar = old_p3 + old_v3*dt
-    p3 = old_p3 + (dt/2)*(2*old_v3+(accelerator(norm, m1, old_p1, p3_bar) + accelerator(norm, m2, old_p2, p3_bar))*dt)
+    p3 = old_p3 + (dt/2)*(2*old_v3+accelerator(norm, m2, old_p2, p3_bar)*dt)
 
     t += dt
 
@@ -90,9 +65,14 @@ def pathFinder(T, dt, venus, swarm):
     # Now use the "velocity verlet" technique to step positions forward
     # from the already determined first two values.
     for i in range(2, round(T/dt)):
-        new_p1 = 2*p1 + (accelerator(norm, m2, p2, p1) + accelerator(norm, m3, p3, p1))*dt**2 - old_p1
-        new_p2 = 2*p2 + (accelerator(norm, m1, p1, p2) + accelerator(norm, m3, p3, p2))*dt**2 - old_p2
-        new_p3 = 2*p3 + (accelerator(norm, m1, p1, p3) + accelerator(norm, m2, p2, p3))*dt**2 - old_p3
+        
+        phase = 2*np.pi*t/p_venus
+
+        r_venus = np.array([1.08208e11*np.cos(phase), 1.08208e11*np.sin(phase)])
+
+        new_p1 = 2*p1 + accelerator(norm, m3, p3, p1)*dt**2 - old_p1
+        new_p2 = 2*p2 + accelerator(norm, m3, p3, p2)*dt**2 - old_p2
+        new_p3 = 2*p3 + accelerator(norm, m2, p2, p3)*dt**2 - old_p3
 
         # Update the position values
         old_p1 = p1
@@ -111,12 +91,10 @@ def pathFinder(T, dt, venus, swarm):
     return paths
 
 start = time.time()
-paths = np.transpose(pathFinder(sim_time, dt, venus, swarm))
+paths = np.transpose(pathFinder(sim_time, dt, swarm))
 end   = time.time()
 elapsed = end-start
 print('Finished in %s seconds.'%(round(elapsed,1)))
-
-return paths
 
 plt.figure(figsize=(8,8))
 plt.axes().set_aspect('equal', 'datalim')
