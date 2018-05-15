5325e17e382ae16478f578eb181a1d5689964a18
Jacob Skinner <skinnej3@wwu.edu>
2018-05-15 12:50:39 -0700

    Code maintainence
    -Removed files for animations
    -added docstrings to functions
    -rearranged functions for more compact script.

--- project/project.py
+++ project/project.py
@@ -1,4 +1,4 @@
-import numpy as np, time, matplotlib.animation as animation, scipy.optimize
+import numpy as np, time, scipy.optimize
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 rcParams['xtick.direction'] = 'in'
@@ -11,6 +11,128 @@ then steps into a velocity verlet loop until complete.
 All internal calculations are in SI units.
 '''
 
+def r_venus(t):
+    '''
+    Calculate the position of venus at time t.
+
+    Parameters
+    ----------
+    t : float
+        Time inside simulation.
+    
+    Returns
+    -------
+    r_venus : numpy array (2)
+        Position vector of Venus.
+    '''
+
+    # position = r(sin(wt), cos(wt))
+
+    r_venus  = 1.08208e11*np.array([np.cos(2*np.pi*t/1.941436081e7),
+                          np.sin(2*np.pi*t/1.941436081e7)])
+    
+    return r_venus
+def accelerator(r, t):
+    '''
+    Calculate the net acceleration (Sol + Venus) that an
+    object at position r feels at time t.
+
+    Parameters
+    ----------
+    r : numpy array (2)
+        Position vector of object.
+    
+    t : float
+        Time inside simulation.
+    
+    Returns
+    -------
+    acceleration : numpy array (2)
+        Acceleration vector felt by object.
+    '''
+
+    # Find the acceleration due to venus
+    venus_sep      = r_venus(t)-r
+    venus_sep_norm = np.linalg.norm(venus_sep)
+    venus_accel    = venus_sep*(6.67428e-11*4.867488088236164e+24)/(venus_sep_norm**3)
+
+    # Find acceleration due to sun
+    sol_sep      = -r
+    sol_sep_norm = np.linalg.norm(sol_sep)
+    sol_accel    = sol_sep*(6.67428e-11*1.989e30)/(sol_sep_norm**3)
+
+    return venus_accel + sol_accel
+def pathFinder(swarm, T, dt):
+    '''
+    Perform gravitational simulation of 2-body Sol-Venus
+    system with additional 'massless' small bodies.
+
+    Parameters
+    ----------
+    swarm : numpy array (n_objects, 4)
+        Initial positions and velocities of all
+        n objects in the swarm.
+    
+    T : float
+        Total simulation time (in Venusian years).
+    
+    dt : float
+        Simulation timestep in seconds.
+    
+    Returns
+    -------
+    paths : numpy array (T/dt, n_objects, 4)
+        Array storing position and velocity
+        coordinates for all n objects at all
+        times within the simulation.
+    '''
+    
+    # Convert T from Venusian years to seconds.
+    T = round(T*31556926*0.615198)
+    
+    # 'paths' stores positions and velocities
+    paths = np.zeros((round(T/dt), swarm.shape[0], 4))
+
+    # Calculate the path for each object - in series.
+    for i in range(swarm.shape[0]):
+        
+        #Store initial values.
+        old_pi = np.array([swarm[i,0], swarm[i,1]])
+        old_vi = np.array([swarm[i,2], swarm[i,3]])
+
+        paths[0,i,0], paths[0,i,1], paths[0,i,2], paths[0,i,3] = swarm[i,0], swarm[i,1], swarm[i,2], swarm[i,3]
+
+
+        # Use Heun's method to find the i+1th value.
+        pi_bar = old_pi + old_vi*dt
+        pi = old_pi + (dt/2)*(2*old_vi + accelerator(pi_bar, 0)*dt)
+
+        paths[1,i,0], paths[1,i,1] = pi[0], pi[1]
+        
+        #Store 1st velocity.
+        v = (pi - old_pi)/dt
+        paths[1,i,2], paths[1,i,3] = v[0], v[1]
+
+        t = dt
+
+        # Use Verlet method to calculate trajectory for i'th object at time 'j'.
+        for j in range(2, round(T/dt)):
+            
+            new_pi = 2*pi + accelerator(pi, t)*dt**2 - old_pi
+
+            # Update the position values
+            old_pi = pi
+            pi = new_pi
+
+            t += dt
+
+            paths[j,i,0], paths[j,i,1] = pi[0], pi[1]
+            
+            #Store subsequent velocity.
+            v = (pi - old_pi)/dt
+            paths[j,i,2], paths[j,i,3] = v[0], v[1]
+    
+    return paths
 def L_points():
     # Because the location of venus is a function of time,
     # the lagrangian points are also functions of time,
@@ -53,88 +175,14 @@ L3 = np.array([-108207845529.61938, 0, 0, -35019.950015715345])
 L4 = np.array([5.41040000e10, 9.37108769e10, -35020.00000788533*np.sqrt(3)/2, 35020.00000788533/2])
 L5 = np.array([5.41040000e10, -9.37108769e10, 35020.00000788533*np.sqrt(3)/2, 35020.00000788533/2])
 
-def r_venus(t):
-    '''
-    Because Venus' position is approximated as perfectly circular,
-    attracted only by the sun, it moves about the sun at constant
-    radius with a constant angular velocity.
-    '''
 
-    # position = r(sin(wt), cos(wt))
-
-    r_venus  = 1.08208e11*np.array([np.cos(2*np.pi*t/1.941436081e7),
-                          np.sin(2*np.pi*t/1.941436081e7)])
-    
-    return r_venus
-
-def accelerator(r, t):
-    # Calculate the acceleration at r due to Sol and Venus.
-
-    # Find the acceleration due to venus
-    venus_sep      = r_venus(t)-r
-    venus_sep_norm = np.linalg.norm(venus_sep)
-    venus_accel    = venus_sep*(6.67428e-11*4.867488088236164e+24)/(venus_sep_norm**3)
-
-    # Find acceleration due to sun
-    sol_sep      = -r
-    sol_sep_norm = np.linalg.norm(sol_sep)
-    sol_accel    = sol_sep*(6.67428e-11*1.989e30)/(sol_sep_norm**3)
-
-    return venus_accel + sol_accel
-#TODO: Make pathfinder loop over each row of a "swarm" array 
-def pathFinder(swarm, T, dt):
-    '''
-    Takes in a run time, delta-t, and info for 3 bodies.
-    Returns timestamped coordinates for the specified run time.
-    '''
-    
-    # Convert T from Venusian years to seconds.
-    T = round(T*31556926*0.615198)
-    
-    paths = np.zeros((round(T/dt), swarm.shape[0], 2))
-
-    # Calculate the path for each object - in series.
-    for i in range(swarm.shape[0]):
-        
-        #Store initial values.
-        old_pi = np.array([swarm[i,0], swarm[i,1]])
-        old_vi = np.array([swarm[i,2], swarm[i,3]])
-
-        paths[0,i,0], paths[0,i,1] = swarm[i,0], swarm[i,1]
-
-
-        # Use Heun's method to find the i+1th value.
-        pi_bar = old_pi + old_vi*dt
-        pi = old_pi + (dt/2)*(2*old_vi + accelerator(pi_bar, 0)*dt)
-
-        paths[1,i,0], paths[1,i,1] = pi[0], pi[1]
-
-
-        t = dt
-
-        # Use Verlet method to calculate trajectory for i'th object.
-        for j in range(2, round(T/dt)):
-            
-            new_pi = 2*pi + accelerator(pi, t)*dt**2 - old_pi
-
-            # Update the position values
-            old_pi = pi
-            pi = new_pi
-
-            t += dt
-
-            paths[j,i,0], paths[j,i,1] = pi[0], pi[1]
-
-            
-    
-    return paths
 
 # Time in Venusian years!
-T = 200
+T = 1000
 dt = 100000
 
 # Swarm shape: (number of objects, coords)
-swarm = np.array([L1])#, L2, L3, L4, L5])
+swarm = np.array([L1, L2, L3, L4])
 
 # Calculate swarm trajectories.
 start = time.time()
@@ -145,43 +193,37 @@ print('Finished in %s seconds.'%(round(elapsed,1)))
 print(paths.shape)
 
 
-# Plot stuff!
-
+# Plot orbital paths
 time = np.linspace(0, T*31556926, round(T*31556926/dt))
 venus = r_venus(time)
 
-
+'''
 plt.figure(figsize=(8,8))
 plt.axes().set_aspect('equal', 'datalim')
 plt.grid()
-#plt.plot(venus[0],venus[1], 'r')
-#plt.scatter(0,0, s=1e3, c='y')
+plt.plot(venus[0],venus[1], label='Venus')
+#plt.scatter(0,0, s=1e3, c='y', label='Sun')
 # Plot the swarm.
 for i in range(paths.shape[1]):
-    plt.plot(paths[::194,i,0], paths[::194,i,1])
-#plt.savefig('Orbital Paths.pdf', bbox_inches='tight')
+    plt.plot(paths[:,i,0], paths[:,i,1], label='L%s'%(i+1))
+plt.legend(fontsize=18)
+plt.savefig('lagrange paths.png', bbox_inches='tight', dpi=300)
 plt.show()
 plt.clf()
-
 '''
-#plt.plot(paths[1], paths[2], lw=0.1)
-plt.plot(paths[3][0::315569], paths[4][0::315569], lw=1)
-#plt.plot(paths[5], paths[6], lw=0.1)
-plt.savefig('Earth Poincare.pdf', bbox_inches='tight')
+
+#Plot Poincare sections
+plt.figure(figsize=(8,8))
+#plt.axes().set_aspect('equal', 'datalim')
+plt.grid()
+plt.plot(venus[0::194],venus[1::194], '.', label='Venus')
+del venus, time
+# Plot the swarm.
+for i in range(paths.shape[1]):
+    p_mag = np.linalg.norm(np.array([paths[::194,i,0], paths[::194,i,1]]))
+    v_mag = np.linalg.norm(np.array([paths[::194,i,2], paths[::194,i,3]]))
+    plt.plot(p_mag, v_mag, '.', label='L%s'%(i+1))
+plt.legend(fontsize=18)
+plt.savefig('poincare.png', bbox_inches='tight', dpi=300)
 plt.show()
-'''
-'''
-# Make an animation
-fig, ax = plt.subplots(figsize=(8,8))
-plt.xlim(-2e11, 2e11)
-plt.ylim(-2e11, 2e11)
-
-def init(): # Clears the frame for the animator.
-    return ax.scatter([],[])
-def animate(i): # Returns frames to the animator.  
-    return ax.scatter(paths[i,0,0], paths[i,0,1], c='k')
-
-ani = animation.FuncAnimation(fig, animate, init_func=init, blit=False, interval = 17)
-
-plt.show()
-'''
+plt.clf()
